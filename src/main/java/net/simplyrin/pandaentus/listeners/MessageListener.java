98b4e0173465eb7e734d22d4cd0537c3809427e5
SimplyRin <simplyrin.dev@gmail.com>
2019-08-01 16:32:21 +0900

    Add !ojichat command.

--- src/main/java/net/simplyrin/pandaentus/listeners/MessageListener.java
+++ src/main/java/net/simplyrin/pandaentus/listeners/MessageListener.java
@@ -259,14 +259,50 @@ public class MessageListener extends ListenerAdapter {
 			}
 
 			if (args[0].equalsIgnoreCase("!ojichat")) {
-				String arg;
-				if (args.length > 1) {
-					arg = "";
-					for (int i = 1; i < args.length; i++) {
-						arg = arg + args[i] + " ";
-					}
+				String name = "";
+				for (int i = 1; i < args.length; i++) {
+					name = name + args[i] + " ";
 				}
 
+				boolean normalText = false;
+
+				name = name.trim();
+
+				// テキトー！！
+				if (name.contains("-normal") || name.contains("-n")) {
+					normalText = true;
+					name = name.replace("-normal", "");
+					name = name.replace("-n", "");
+				}
+
+				name = name.trim();
+
+				HttpClient httpClient = new HttpClient("https://ojichat.appspot.com/post");
+				httpClient.setData("name=" + name + "&emoji_level=4&punctuation_level=0");
+
+				httpClient.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36");
+
+				httpClient.addHeader("Accept", "application/json, text/plain, */*");
+				httpClient.addHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+				httpClient.addHeader("Origin", "https://oji.netlify.com");
+				httpClient.addHeader("Referer", "https://oji.netlify.com/");
+
+				String result;
+				try {
+					result = httpClient.getAsJsonObject().get("message").getAsString();
+				} catch (Exception e) {
+					return;
+				}
+
+				embedBuilder.setColor(Color.GREEN);
+				embedBuilder.setDescription(result);
+
+				if (normalText) {
+					channel.sendMessage(result).complete();
+				} else {
+					channel.sendMessage(embedBuilder.build()).complete();
+				}
+				return;
 			}
 		}
 	}
