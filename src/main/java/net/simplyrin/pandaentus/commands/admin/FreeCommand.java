1e12b08ebf1a9ff1532b6e272d1804d137bcb7fc
SimplyRin <simplyrin.dev@gmail.com>
2020-07-09 23:28:32 +0900

    Add !free command

---
+++ src/main/java/net/simplyrin/pandaentus/commands/admin/FreeCommand.java
@@ -0,0 +1,116 @@
+package net.simplyrin.pandaentus.commands.admin;
+
+import java.awt.Color;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.simplyrin.pandaentus.Main;
+import net.simplyrin.pandaentus.utils.BaseCommand;
+import net.simplyrin.pandaentus.utils.CommandType;
+import net.simplyrin.processmanager.Callback;
+import net.simplyrin.processmanager.ProcessManager;
+
+/**
+ * Created by SimplyRin on 2020/07/09.
+ *
+ * Copyright (c) 2020 SimplyRin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+public class FreeCommand implements BaseCommand {
+
+	@Override
+	public String getCommand() {
+		return "!free";
+	}
+
+	@Override
+	public CommandType getType() {
+		return CommandType.EqualsIgnoreCase;
+	}
+
+	@Override
+	public void execute(Main instance, MessageReceivedEvent event, String[] args) {
+		User user = event.getAuthor();
+
+		if (!user.getId().equals(instance.getAdminId())) {
+			return;
+		}
+
+		MessageChannel channel = event.getChannel();
+
+		ProcessManager.runCommand(new String[] { "free", "-h" }, new Callback() {
+			int i = 0;
+
+			String memTotal, memUsed, memFree, swapTotal, swapUsed, swapFree;
+
+			@Override
+			public void line(String line) {
+				System.out.println(line);
+
+				if (line.startsWith("Mem:")) {
+					String[] args = line.trim().replaceAll(" +", " ").split(" ");
+					int i = 0;
+					for (String arg : args) {
+						System.out.println("args[" + i + "] -> " + arg);
+						i++;
+					}
+					this.memTotal = args[1];
+					this.memUsed = args[2];
+					this.memFree = args[3];
+
+					this.i = 2;
+				}
+
+				else if (line.startsWith("Swap")) {
+					String[] args = line.trim().replaceAll(" +", " ").split(" ");
+					int i = 0;
+					for (String arg : args) {
+						System.out.println("args[" + i + "] -> " + arg);
+						i++;
+					}
+					this.swapTotal = args[1];
+					this.swapUsed = args[2];
+					this.swapFree = args[3];
+				}
+			}
+
+			@Override
+			public void processEnded() {
+				try {
+					Thread.sleep(100);
+				} catch (Exception e) {
+				}
+				EmbedBuilder embedBuilder = new EmbedBuilder();
+				embedBuilder.setColor(Color.YELLOW);
+				embedBuilder.setAuthor("Ram usage");
+				embedBuilder.addField("Total", this.memTotal, true);
+				embedBuilder.addField("Used", this.memUsed, true);
+				embedBuilder.addField("Free", this.memFree, true);
+				embedBuilder.addField("Swap Total", this.swapTotal, true);
+				embedBuilder.addField("Swap Used", this.swapUsed, true);
+				embedBuilder.addField("Swap Free", this.swapFree, true);
+				channel.sendMessage(embedBuilder.build()).complete();
+			}
+		}, true);
+	}
+
+}
--- src/main/java/net/simplyrin/pandaentus/utils/Version.java
+++ src/main/java/net/simplyrin/pandaentus/utils/Version.java
@@ -23,7 +23,7 @@ package net.simplyrin.pandaentus.utils;
 public class Version {
 
 	public static final String BUILD_NUMBER = "";
-	public static final String BUILD_TIME  ="2020-07-09 20:30:26";
+	public static final String BUILD_TIME  ="2020-07-09 20:47:47";
 	public static final String POMVERSION = "1.2";
 
 }
