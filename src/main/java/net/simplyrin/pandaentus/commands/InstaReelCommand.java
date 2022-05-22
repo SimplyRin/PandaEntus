f9bc35613f65fe275f539ee5fbd539ef283af9f1
SimplyRin <github@simplyrin.net>
2022-05-22 14:43:07 +0900

    Update InstaReelCommand.java

--- src/main/java/net/simplyrin/pandaentus/commands/InstaReelCommand.java
+++ src/main/java/net/simplyrin/pandaentus/commands/InstaReelCommand.java
@@ -1,14 +1,9 @@
 package net.simplyrin.pandaentus.commands;
 
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.Charset;
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 import java.util.regex.Pattern;
-
-import javax.net.ssl.HttpsURLConnection;
-
-import org.apache.commons.io.IOUtils;
 
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.simplyrin.pandaentus.PandaEntus;
@@ -16,6 +11,8 @@ import net.simplyrin.pandaentus.classes.BaseCommand;
 import net.simplyrin.pandaentus.classes.CommandPermission;
 import net.simplyrin.pandaentus.classes.CommandType;
 import net.simplyrin.pandaentus.classes.PandaMessageEvent;
+import net.simplyrin.processmanager.Callback;
+import net.simplyrin.processmanager.ProcessManager;
 
 /**
  * Created by SimplyRin on 2020/11/25.
@@ -38,7 +35,7 @@ import net.simplyrin.pandaentus.classes.PandaMessageEvent;
 public class InstaReelCommand implements BaseCommand {
 
 	public static void main(String[] args) {
-		new InstaReelCommand().execute(null, null, new String[] { "https://www.instagram.com/reel/..." });
+		new InstaReelCommand().execute(null, null, new String[] { "https://www.instagram.com/reel/CbxCszjp4WL/" }); // ←かわいい
 	}
 	
 	@Override
@@ -77,45 +74,27 @@ public class InstaReelCommand implements BaseCommand {
 		if (url.contains(Pattern.quote("?"))) {
 			url = url.split(Pattern.quote("?"))[0];
 		}
+		
+		var id = UUID.randomUUID().toString().split("-")[0];
 
-		System.out.println("[InstaReelCommand] Checking: " + url);
-
-		HttpsURLConnection connection = null;
-		try {
-			connection = (HttpsURLConnection) new URL(url).openConnection();
-		} catch (Exception e) {
-			e.printStackTrace();
+		System.out.println("[InstaReelCommand] [" + id + "] Checking: " + url);
+		
+		String youtubeDlPathTemp = "youtube-dl";
+		File youtubeDl = new File("youtube-dl");
+		if (youtubeDl.exists()) {
+			youtubeDlPathTemp = youtubeDl.getAbsolutePath();
 		}
-		connection.addRequestProperty("accept", "*/*");
-		connection.addRequestProperty("accept-encoding", "identity;q=1, *;q=0");
-		connection.addRequestProperty("accept-language", "ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7");
-		connection.addRequestProperty("cache-control", "no-cache");
-		connection.addRequestProperty("pragma", "no-cache");
-		connection.addRequestProperty("range", "bytes=0-");
-		connection.addRequestProperty("referer", "https://www.instagram.com/");
-		connection.addRequestProperty("sec-fetch-dest", "video");
-		connection.addRequestProperty("sec-fetch-dest", "no-cors");
-		connection.addRequestProperty("sec-fetch-site", "cross-site");
-		connection.addRequestProperty("user-agent", instance.getBotUserAgent());
-
-		String result = null;
-		try {
-			connection.connect();
-			result = IOUtils.toString(connection.getInputStream(), Charset.defaultCharset());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		if (result != null && result.contains("property=\"og:video\" content=\"")) {
-			String videoUrl = result.split(Pattern.quote("property=\"og:video\" content=\""))[1].split(Pattern.quote("/>"))[0];
-			videoUrl = videoUrl.replace("\"", "");
-			videoUrl = videoUrl.trim();
-
-			System.out.println(videoUrl);
-			event.getChannel().sendMessage(videoUrl).complete();
-		} else {
-			System.out.println("検出できませんでした。");
-		}
+		
+		ProcessManager.runCommand(new String[]{ youtubeDlPathTemp, "--get-url", url }, new Callback() {
+			@Override
+			public void line(String response) {
+				System.out.println("[InstaReel] [" + id + "] " + response);
+				
+				if (response.toLowerCase().startsWith("https://")) {
+					event.getChannel().sendMessage(response).complete();
+				}
+			}
+		}, true);
 	}
 
 }
