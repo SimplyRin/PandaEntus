944e4ca150b937b2a542fabf290401e20d46e5c2
SimplyRin <github@simplyrin.net>
2025-08-20 20:27:55 +0900

    Create SameCommand.java

---
+++ src/main/java/net/simplyrin/pandaentus/commands/SameCommand.java
@@ -0,0 +1,61 @@
+package net.simplyrin.pandaentus.commands;
+
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.internal.interactions.CommandDataImpl;
+import net.simplyrin.pandaentus.PandaEntus;
+import net.simplyrin.pandaentus.classes.BaseCommand;
+import net.simplyrin.pandaentus.classes.CommandPermission;
+import net.simplyrin.pandaentus.classes.CommandType;
+import net.simplyrin.pandaentus.classes.PandaMessageEvent;
+
+/**
+ * Created by SimplyRin on 2025/08/20.
+ *
+ * Copyright (C) 2025 SimplyRin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+public class SameCommand extends BaseCommand {
+
+	@Override
+	public String getCommand() {
+		return "!same";
+	}
+	
+	@Override
+	public String getDescription() {
+		return "さめ";
+	}
+	
+	@Override
+	public CommandData getCommandData() {
+		return new CommandDataImpl("same", this.getDescription());
+	}
+
+	@Override
+	public CommandType getType() {
+		return CommandType.EqualsIgnoreCase;
+	}
+
+	@Override
+	public CommandPermission getPermission() {
+		return CommandPermission.Everyone;
+	}
+
+	@Override
+	public void execute(PandaEntus instance, PandaMessageEvent event, String[] args) {
+		event.reply("https://api.rin.pink/pandaentus/same.jpg");
+	}
+
+}
